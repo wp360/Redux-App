4b35e5098900f08596582bbb9def799c5c31b47e
wp360 <2897406527@qq.com>
2017-07-02 18:04:12 +0800

    add new

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+
 let app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*',(req,res)=>{
     //res.send('hello world');
