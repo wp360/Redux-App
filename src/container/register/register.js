8808f1672bfd48515a3e1a16c0dfa8777301a7c5
wp360 <2897406527@qq.com>
2020-05-04 00:48:59 +0800

    高阶组件使用

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -11,29 +11,35 @@ import {
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import {register} from '../../redux/user.redux'
+import Form from '../../component/form/form'
 
 @connect(
   state=>state.user,
   {register}
 )
+
+@Form
 class Register extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {
-      user: '',
-      pwd: '',
-      repeatpwd: '',
-      type: 'genius' // 或者boss
-    }
+    // this.state = {
+    //   user: '',
+    //   pwd: '',
+    //   repeatpwd: '',
+    //   type: 'genius' // 或者boss
+    // }
     this.handleRegister = this.handleRegister.bind(this)
   }
-  handleChange(key,val){
-    this.setState({
-      [key]: val
-    })
+  componentDidMount(){
+    this.props.handleChange('type', 'genius')
   }
+  // handleChange(key,val){
+  //   this.setState({
+  //     [key]: val
+  //   })
+  // }
   handleRegister(){
-    this.props.register(this.state)
+    this.props.register(this.props.state)
     // console.log(this.state)
   }
   render() {
@@ -49,14 +55,14 @@ class Register extends React.Component{
         <WingBlank>
           <List>
             {this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
-            <InputItem onChange={v=>this.handleChange('user',v)}>用户名</InputItem>
+            <InputItem onChange={v=>this.props.handleChange('user',v)}>用户名</InputItem>
             <WhiteSpace />
-            <InputItem type="password" onChange={v=>this.handleChange('pwd',v)}>密码</InputItem>
+            <InputItem type="password" onChange={v=>this.props.handleChange('pwd',v)}>密码</InputItem>
             <WhiteSpace />
-            <InputItem type="password" onChange={v=>this.handleChange('repeatpwd',v)}>确认密码</InputItem>
+            <InputItem type="password" onChange={v=>this.props.handleChange('repeatpwd',v)}>确认密码</InputItem>
             <WhiteSpace />
-            <RadioItem checked={this.state.type === 'genius'} onChange={()=>this.handleChange('type','genius')}>人才</RadioItem>
-            <RadioItem checked={this.state.type === 'boss'} onChange={()=>this.handleChange('type','boss')}>Boss</RadioItem>
+            <RadioItem checked={this.props.state.type === 'genius'} onChange={()=>this.props.handleChange('type','genius')}>人才</RadioItem>
+            <RadioItem checked={this.props.state.type === 'boss'} onChange={()=>this.props.handleChange('type','boss')}>Boss</RadioItem>
             <WhiteSpace />
             <Button type='primary' onClick={this.handleRegister}>注册</Button>
           </List>
