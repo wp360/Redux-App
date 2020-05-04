06914187cebc7d274e4652f8310e5e1ce62ed8cd
wp360 <2897406527@qq.com>
2020-05-04 09:42:43 +0800

    socket.io前后端联通

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const userRouter = require('./user')
 
 // 新建app
 const app = express()
+
+// socket.io绑定express
+// work with express
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', function(socket) {
+  console.log('用户登录')
+})
+
+const userRouter = require('./user')
+
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user', userRouter)
@@ -13,6 +24,6 @@ app.get('/',function(req,res){
   res.send('<h1>Hello React</h1>')
 })
 
-app.listen(5000,function(){
+server.listen(5000,function(){
   console.log('Node App Start at Port 5000')
 })
